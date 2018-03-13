645074a747b8b9e71ff0ff74901a64c86215be6e
Maria Martinez <mariabrea75@marias-mac-mini>
2018-03-13 11:27:31 -0500

    Initial Commit

---
+++ MasterWords/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MasterWords
+//
+//  Created by Maria Martinez on 3/13/18.
+//  Copyright © 2018 Maria Martinez Guzman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
